a1e142453ac672536e15491021f0d623b0448745
Amey <apotnis@equalexperts.com>
2016-07-14 13:39:28 +0530

    project setup

---
+++ attendance-service /src/test/java/org/campus/AttendanceServiceApplicationTests.java
@@ -0,0 +1,18 @@
+package org.campus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AttendanceServiceApplication.class)
+@WebAppConfiguration
+public class AttendanceServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
